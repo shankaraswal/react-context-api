fbe120cb672134230eef4a2c6c0911c3f6f3777d
ShankarAswal <shankaraswal@gmail.com>
2026-09-13 00:41:14 +0100

    Update Next.js configuration options
    Set output format to 'export', define base path, and disable image optimization for static export.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/react-context-api',
+  images: {
+    unoptimized: true, // Next.js Image Optimization को static export के लिए disable करने हेतु
+  },
 };
 
 export default nextConfig;
